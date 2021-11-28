383fd058db60d08f0a89856b18fe6ea2a8de8eab
Shreshth Tuli <shreshthtuli@gmail.com>
2021-11-28 19:17:12 +0000

    Updated host stat and details calls

--- serverless/agent/agent.py
+++ serverless/agent/agent.py
@@ -5,21 +5,29 @@ import subprocess
 import os
 import sys
 
+def parse_io(line):
+    val = float(line.split(" ")[-2])
+    unit = line.split(" ")[-1]
+    if 'G' in unit: val *= 1000
+    elif 'K' in unit: val /= 1000
+    return val * 1.048576
+
 def hostDetails():
     memory = psutil.virtual_memory()
     disk = psutil.disk_usage('/')
-    data = subprocess.run("~/scripts/probe.sh", shell=True,stdout=subprocess.PIPE)
+    data = subprocess.run("~/agent/probe.sh", shell=True,stdout=subprocess.PIPE)
     data  = (data.stdout.decode()).splitlines()
-    bw = ((subprocess.run("sudo ethtool "+self.interface+" | grep Speed",shell=True,stdout=subprocess.PIPE)).stdout.decode()).split()[1][0:4]
+    interface = 'eth0'
+    bw = ((subprocess.run("sudo ethtool "+interface+" | grep Speed",shell=True,stdout=subprocess.PIPE)).stdout.decode()).split()[1][0:4]
     payload ={
             "Total_Memory": int(float(memory.total/(1024*1024))),
             "Total_Disk": int(float(disk.total/(1024*1024))),
             "Bandwidth": int(bw),
             "clock": data[0],
-            "Ram_read": self.parse_io(data[3]),
-            "Ram_write": self.parse_io(data[4]),
-            "Disk_read": self.parse_io(data[1]),
-            "Disk_write": self.parse_io(data[2])}
+            "Ram_read": parse_io(data[3]),
+            "Ram_write": parse_io(data[4]),
+            "Disk_read": parse_io(data[1]),
+            "Disk_write": parse_io(data[2])}
     return json.dumps(payload)
 
 def gethostStat():
